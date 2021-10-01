396ba2003bbda8504b1a1dcf526d85c38589c22c
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-01 18:38:45 -0300

    Corrigindo import at DATE

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
-import Date from '../components/date'
+import Date from '../../components/date'
 
 export default function Post({ postData }) {
   return (
